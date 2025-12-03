27f7049d6fc2309a0bdbe71a5620b0276df04020
Vivek0925 <vivekmandal123v@gmail.com>
2025-12-03 16:20:35 +0530

    login page

--- app/page.js
+++ app/page.js
@@ -26,37 +26,43 @@ export default function Home() {
 
       <div className="main flex flex-col justify-center items-center p-40 gap-4 text-black ">
 
-        <div className="flex gap-5 text-4xl font-extrabold">Buy Me A Cake <span><img src="cake1.gif" width="40" alt="" /></span> </div>  
+        <div className="flex gap-5 text-4xl font-extrabold">Buy Me A Cake <span><img src="dance-cake.gif" width="40" alt="" /></span> </div>  
         <p>A Crowd funding platform for your Creators. Get Funded by your Fans and Followers!</p> 
 
 
       <div className="flex justify-center items-center gap-6">
-        <button type="button" className="text-white bg-linear-to-br from-purple-600 to-blue-500 hover:bg-linear-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5">Start Here</button>
+        <button type="button" className="text-white  rounded-2xl bg-linear-to-br from-purple-600 to-blue-500 hover:bg-linear-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5">Start Here</button>
 
-        <button type="button" className="text-white bg-linear-to-br from-purple-600 to-blue-500 hover:bg-linear-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5">Get Started</button>
+        <button type="button" className="text-white rounded-2xl bg-linear-to-br from-purple-600 to-blue-500 hover:bg-linear-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5">Get Started</button>
       </div>
 
       </div>
       <div>
 
       <div className="line bg-blue-900 h-1 opacity-10"></div>
-      <div>
-        <h1 className="text-center font-bold p-7 text-2xl">Your fans can buy you a cake!</h1>
+      <div className="text-black container p-12">
+        <h1 className="text-center font-bold text-2xl">Your fans can buy you a cake!</h1>
         <div className="flex gap-5 justify-around ">
-          <div className="items">
-            <Image src="/cake1.gif" width={100} height={100} alt="Cake" />
-            <p className="font-bold">Fund Yourself</p>
+          <div className="items flex flex-col items-center justify-center">
+              <Image className="bg-slate-500 rounded-full" src="/dance-cake.gif" width={100} height={100} alt="Cake" />
+            <p className="font-bold">fans want to help you</p>
+            <p className="">Your fans are here to help you</p>
           </div>
-          <div className="items">
-            <Image src="/cake1.gif" width={100} height={100} alt="Cake" />
+            <div className="items flex flex-col items-center justify-center py-8">
+            <Image className=" bg-slate-500 rounded-full" src="/dance-cake.gif" width={100} height={100} alt="Cake" />
             <p className="font-bold">Fund Yourself</p>
+              <p className="">Your fans are here to help you</p>
           </div>
-          <div className="items">
-            <Image src="/cake1.gif" width={100} height={100} alt="Cake" />
+            <div className="items flex flex-col items-center justify-center">
+            <Image className="bg-slate-500 rounded-full" src="/dance-cake.gif" width={100} height={100} alt="Cake" />
             <p className="font-bold">Fund Yourself</p>
+              <p className="">Your fans are here to help you</p>
           </div>
         </div>
       </div>
+
+        <div className="line bg-blue-900 h-1 opacity-10"></div>
+        
       </div>
     </>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <nav className='fixed top-0 left-0 w-full z-50 transition-all duration-300 bg-transparent text-white flex justify-around h-16 items-center'>
+    <nav className='fixed top-0 left-0 w-full  bg-slate-800/30 backdrop-blur-md transition-all duration-300 text-white flex justify-around h-16 items-center'>
         <div className="logo text-2xl font-extrabold">Get-Me-A-Cake</div>
         <ul className='flex justify-between gap-6'>
             <li>Home</li>
             <li>About</li>
             <li>Contact</li>
-            <li className='border px-2'>Find a Creator</li>
-            <li className='border px-2'>Login</li>
+            <li className='border px-2 rounded-2xl'>Find a Creator</li>
         </ul>
+
+        <div>
+          <Link href = {"/login"}>
+        <button type="button" className="text-white  rounded-2xl bg-linear-to-br from-purple-600 to-blue-500 hover:bg-linear-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5">Login</button>
+          </Link>
+        </div>
     </nav>
   )
 }
