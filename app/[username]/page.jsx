36fbec08bda14f7302e6738cfd8748db39b51d84
Vivek0925 <vivekmandal123v@gmail.com>
2026-02-22 17:44:04 +0530

    added supporters and payment components

--- app/[username]/page.jsx
+++ app/[username]/page.jsx
@@ -13,19 +13,62 @@ export async function Username({ params }) {
         </div>
         <div className="info flex justify-center items-center my-13 flex-col">
 
-          <div className='font-bold'> 
-          @{username}
+          <div className='font-bold'>
+            @{username}
           </div>
 
           <div className='text-slate-500'>
             Creating a podcast with ben avery, devan costa and jace avery
-            </div>
+          </div>
 
-            <div className='text-slate-500'>
+          <div className='text-slate-500'>
             12,348 members . 459 posts . $43,690/month
           </div>
         </div>
 
+
+
+        <div className="payment flex p-4 gap-3 w-full">
+          <div className="supporters w-1/2 bg-amber-200/20 rounded-lg p-8">
+            <h2 className='text-lg font-bold my-3'>Supporters</h2>
+            <ul className='mx-5'>
+              <li className='flex gap-3 my-3 items-center'>
+                <img width={37} src="avatar.gif" alt="" />
+                akash donated 50$ with a message ""
+              </li>
+              <li className='flex gap-3 my-3 items-center'>
+                <img width={37} src="avatar.gif" alt="" />
+                akash donated 50$ with a message ""
+              </li>
+              <li className='flex gap-3 my-3 items-center'>
+                <img width={37} src="avatar.gif" alt="" />
+                akash donated 50$ with a message ""
+              </li>
+              <li className='flex gap-3 my-3 items-center'>
+                <img width={37} src="avatar.gif" alt="" />
+                akash donated 50$ with a message ""
+              </li>
+            </ul>
+          </div>
+
+          <div className="makepayment w-1/2 bg-blue-300/20 rounded-lg p-8">
+            <h2 className='text-lg font-bold my-3'>Buy a cake for this creator</h2>
+            <div className='flex flex-col gap-2'>
+              {/* input for name and message optional */}
+              <input type="text" placeholder='Enter your name' className='p-3 w-full rounded-lg bg-slate-300' />
+              <input type="text" placeholder='Enter a message ' className='p-3 w-full rounded-lg bg-slate-300' />
+              <input type="number" placeholder='Enter Amount in USD' className='p-3 w-full rounded-lg bg-slate-300' />
+              <button className=' rounded-lg bg-blue-300 p-3 '>Pay</button>
+            </div>
+            {/* or choose an amount */}
+            <div className='flex gap-3 mt-3'>
+              <button className=' rounded-lg bg-blue-300 p-3 hover:cursor-cell '>05$</button>
+              <button className=' rounded-lg bg-blue-300 p-3 hover:cursor-cell '>10$</button>
+              <button className=' rounded-lg bg-blue-300 p-3 hover:cursor-cell '>20$</button>
+            </div>
+          </div>
+
+        </div>
       </div>
     </>
   );
